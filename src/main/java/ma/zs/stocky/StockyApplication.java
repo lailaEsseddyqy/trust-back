a7aaeaf30bf08c67aba333bdee2f78d6fb307a73
amalfamous <amalfms@gmail.com>
2024-05-22 22:43:44 +0100

    mail1

--- src/main/java/ma/zs/stocky/StockyApplication.java
+++ src/main/java/ma/zs/stocky/StockyApplication.java
@@ -27,15 +27,32 @@ import ma.zs.stocky.zynerator.security.service.facade.*;
 import ma.zs.stocky.zynerator.security.bean.User;
 import ma.zs.stocky.zynerator.security.bean.Role;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import javax.mail.MessagingException;
+
 @SpringBootApplication
 //@EnableFeignClients
 public class StockyApplication {
     public static ConfigurableApplicationContext ctx;
 
+    @Autowired
+    private EmailSenderService senderService;
+
     public static void main(String[] args) {
         ctx=SpringApplication.run(StockyApplication.class, args);
     }
+    @EventListener(ApplicationReadyEvent.class)
+    public void triggerMail() throws MessagingException {
+        senderService.sendSimpleEmail("lailaesseddyqy@gmail.com",
+                "This is email body",
+                "This is email subject");
 
+    }
 
     @Bean
     RestTemplate restTemplate() {
